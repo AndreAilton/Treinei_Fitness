83eb86030136c9e63935ede181288b5c2a2bd4e9
Andre Ailton <andre.ailtonf.10@gmail.com>
2025-08-18 21:12:35 -0300

    Adicionado link dinamicos server.js e urls

--- src/server.js
+++ src/server.js
@@ -2,25 +2,10 @@ import App from "./app.js";
 import dotenv from "dotenv";
 dotenv.config();
 
-import os from "os";
-
-function getLocalIP() {
-  const nets = os.networkInterfaces();
-  for (const name of Object.keys(nets)) {
-    for (const net of nets[name]) {
-      if (net.family === "IPv4" && !net.internal) {
-        return net.address; // retorna o IP real
-      }
-    }
-  }
-  return "localhost"; // fallback
-}
-
-
 const isDocker = process.env.NODE_ENV === "docker";
 
 if (isDocker) {
-  const host = getLocalIP();
+  const host = process.env.API_HOST;
   const portname = process.env.API_PORT;
   const port = process.env.PORT || 3000;
   App.listen(port, () => console.log(`http://${host}:${portname}`));
