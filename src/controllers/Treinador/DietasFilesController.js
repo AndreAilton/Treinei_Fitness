de6364420696eba974ae9ced546c897b7afd87e6
Andre Ailton <andre.ailtonf.10@gmail.com>
2025-12-16 13:36:57 -0300

    Adicionado Url nos Videos

--- src/controllers/Treinador/DietasFilesController.js
+++ src/controllers/Treinador/DietasFilesController.js
@@ -88,6 +88,7 @@ class DietasFilesController {
           "descricao",
           "mime_type",
           "id_treinador",
+          "url",
           "status",
           "created_at",
           "updated_at",
